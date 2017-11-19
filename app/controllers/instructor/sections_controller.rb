8a6d7de728ae1322d5c5ad2da84c5ec25b71170d
elyse.glickman <ejglickmanmedia@gmail.com>
2017-11-19 21:12:51 +0000

    lessonsadd

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,21 +1,2 @@
 class Instructor::SectionsController < ApplicationController
-    def new
-        @course = Course.find(params[:course_id])
-        @section = Section.new
-    end
-
-
-private
-
-  def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
-  end
-
-  private
-
-  def section_params
-    params.require(:section).permit(:title)
-  end
 end
