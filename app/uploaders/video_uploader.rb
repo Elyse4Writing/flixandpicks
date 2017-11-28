1f3c402c7e787a0b409fcd802d860291c0de8573
elyse.glickman <ejglickmanmedia@gmail.com>
2017-11-28 20:44:23 +0000

    lesson23

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,13 +1,14 @@
-class ImageUploader < CarrierWave::Uploader::Base
-  
+class VideoUploader < CarrierWave::Uploader::Base
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-  
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
   #storage :file
-  storage :aws 
- 
-   # Override the directory where uploaded files will be stored.
+    storage :aws
+
+  # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -22,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [800, 350]
+  # process scale: [200, 300]
   #
   # def scale(width, height)
   #   # do something
@@ -32,9 +33,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
-
-end
-
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
@@ -48,4 +46,4 @@ end
   #   "something.jpg" if original_filename
   # end
 
-
+end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,25 @@
 # config/initializers/carrierwave.rb
 
+
+
+# config/initializers/carrierwave.rb
+
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV[AKIAISEFZBDVIRTZM7WQ],   # required
-    aws_secret_access_key: ENV[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],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV[glickman-elyse-user]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
+
+#CarrierWave.configure do |config|
+  #config.fog_provider = 'fog/aws'                        # required
+  #config.fog_credentials = {
+    #provider:              'AWS',                        # required
+    #aws_access_key_id:     ENV[AKIAISEFZBDVIRTZM7WQ],   # required
+    #aws_secret_access_key: ENV[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],        # required
+ #}
+  #config.fog_directory  = ENV[glickman-elyse-user]              # required
+#end
