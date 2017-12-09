97998207441fd9f26f211a1b4bef24c0928cb7b7
elyse.glickman <ejglickmanmedia@gmail.com>
2017-12-09 00:48:29 +0000

    chapter24repeat

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,16 @@
 # config/initializers/carrierwave.rb
 
-
-
-# config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
+  config.storage      = :aws                       # required
+  config.aws_bucket   = ENV["AWS_BUCKET"]   
+  config.aws_acl      = "public-read"
+  
+  config.aws_credentials = {
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    region:                ENV["AWS_REGION"]
+    
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
 
 #CarrierWave.configure do |config|
