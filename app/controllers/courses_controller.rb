f899ebf91260136aaadadd05cdd453e721a8cade
elyse.glickman <ejglickmanmedia@gmail.com>
2017-12-14 23:44:32 +0000

    lesson28challengeone

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,7 @@
 class CoursesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+  
     def index
         @courses = Course.all
     end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+  
   def show
   end
 
@@ -8,4 +11,13 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-end
+  
+  def current_course
+	  @current_course ||= current_lesson.section.course
+  end
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user && current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'You must be enrolled in the course to sign up for this lesson.'
+    end
+  end
